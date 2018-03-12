8a44c10778c11c0c5bf5b80c9b8e6c993c24e646
Sanket Tajane <sanket.tajane@gmail.com>
2018-03-12 18:48:51 -0500

    add proper js file

---
+++ src/IsPrimeNumber.js
@@ -0,0 +1,20 @@
+export function isPrimeNumber (num) {
+  let isPrime = true;
+  if (num >= 2) {
+    if (num === 2 || num === 3) {
+      isPrime = true;
+    } else if (num % 2 === 0) {
+      isPrime = false;
+    } else {
+      for (let i = 3; i <= Math.floor(Math.sqrt(num)); i += 2) {
+        if (num % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+    }
+  } else {
+    isPrime = false;
+  }
+  return isPrime;
+}
